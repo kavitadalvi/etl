5ed8eff4ff27dad9eb2542e4dcb755173a788f2d
KD <kavita.dalvi@gmail.com>
2021-06-20 21:59:51 -0400

    Generalising the transformation process to generate summary data depending on the transform input/output configuration

--- etl.py
+++ etl.py
@@ -2,7 +2,7 @@
 simple program to perform ETL process based on config
 '''
 import argparse
-import logging
+import logging as log
 import sys
 import yaml
 from pipeline import Pipeline, Extract, Transform
@@ -11,44 +11,37 @@ def setup_logging(app_config):
     '''
     Setup logging
     '''
-    logging.basicConfig(
-        filename=app_config['logging']['log_file'],
-        level=app_config['logging']['log_level'],
-        format='%(asctime)s.%(msecs)03d %(module)-10s(%(funcName)-20s) %(levelname)-8s %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-        )
+    log_filename=app_config['logging']['log_file']
+    log_level=app_config['logging']['log_level']
+    log_format='%(asctime)s.%(msecs)03d %(module)-10s(%(funcName)-20s) %(levelname)-8s %(message)s'
+    date_format='%Y-%m-%d %H:%M:%S'
+    log.basicConfig(filename=log_filename,level=log_level,format=log_format,datefmt=date_format)
 
-def main(transform_name, source_file):
+def main(transform_name):
     '''
     Main program to run required ETL pipeline
     '''
-    logging.info('----- Program started -----')
-    logging.info('Transformation name --> %s', transform_name)
-    logging.info('Source file --> %s', source_file)
-    pipeline = Pipeline(transform_name, source_file)
+    log.info('----- Program started -----')
+    log.info('Transformation name --> %s', transform_name)
+    pipeline = Pipeline(transform_name)
     pipeline.get_config()
-    pipeline.get_preprocess_tasks(pipeline.config)
+    pipeline.configure_preprocess_checks()
     extract = Extract(pipeline)
     extract.extract()
     transform = Transform(pipeline,extract)
     transform.transform()
-    if transform_name == "sales-summary":
-        data = transform.gen_sales_summary()
-    if transform_name == "sales-aggregate":
-        data = transform.gen_sales_aggregate()
+    data = transform.gen_output()
     transform.write_json(data)
     transform.write_to_db(data)
-    logging.info( "----- Program complete -----\n\n" )
+    log.info( "----- Program complete -----\n\n" )
 
 if __name__ == "__main__":
     # Parse input arguments
     argsp = argparse.ArgumentParser()
     argsp.add_argument( '-n', '--name',type=str, required=True, help='Transformation name')
-    argsp.add_argument( '-f', '--source',type=str, required=True, help='Source data file')
     argsp.add_argument( '-c', '--config', type=str, required=True, help='Application config file')
     args = argsp.parse_args()
     TRANSFORM_NAME = str(vars(args)['name'])
-    SOURCE_FILE = str(vars(args)['source'])
     APP_CFG_FILE = str(vars(args)['config'])
     APP_CONFIG = {}
     try:
@@ -58,5 +51,6 @@ if __name__ == "__main__":
         print('{} not found'.format(APP_CFG_FILE))
         sys.exit(1)
     else:
+        # setup logging and kickoff transformation process
         setup_logging(APP_CONFIG)
-        main(TRANSFORM_NAME, SOURCE_FILE)
+        main(TRANSFORM_NAME)
